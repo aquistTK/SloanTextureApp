3cabe749c26d6a73f7956a64ece698b341236b25
aquistTK <aquist.ext@threekit.com>
2023-11-10 00:34:40 -0500

    initial dev work

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 async function process() {
     let image = await IJS.Image.load(document.getElementById('color').src);
 
-    let grey = image.crop({width:image.height, height:image.height});
+    var w = image.width;
+    var l = image.height;
 
-    document.getElementById('result').src = grey.toDataURL();
+    var shortest = Math.min(w,l);
+    var longest = Math.max(w,l);
+
+    var isSquare = (l === w);
+
+    var transformX = 0;
+    var transformY = 0;
+
+    var originalText = document.getElementById('originalImageLabel');
+    var resultText = document.getElementById('resultImageLabel');
+
+    if(!isSquare){
+      if(longest == w){
+        transformX = Math.floor((w * .5) - (shortest * .5));
+      }else{
+        transformY = Math.floor((l * .5) - (shortest * .5));
+      }
+
+      originalText.innerHTML = "Not square";
+
+      var result = image.crop({width:shortest, height:shortest, x:transformX, y:transformY});
+      document.getElementById('result').src = result.toDataURL();
+
+      resultText.innerHTML = "Use this cropped version:";
+    }else{
+      originalText.innerHTML = "Image is square";
+      resultText.innerHTML = "";
+      document.getElementById('result').src = "";
+    }
+
+
+
   }
+
+window.myNameValues = {"category": "corian", "name": "", "size": "5", "roughness":"50"};
+
+function setProperty(item, propertyName){
+  window.myNameValues[propertyName] = item.options[item.selectedIndex].value;
+  updateName();
+}
+
+function setBasicProperty(item, propertyName){
+  window.myNameValues[propertyName] = item.value;
+  updateName();
+}
+
+function updateName(){
+  if(window.myNameValues.name != "" && window.myNameValues.name != " "){
+    const nameVal = window.myNameValues;
+    var nameDisplay = document.getElementById('propertyString');
+    nameDisplay.innerHTML = `${nameVal["category"]}_${nameVal["name"]}_${nameVal["size"]}_${nameVal["roughness"]}`;
+  }
+}
+
+
+function copyNameStr() {
+  navigator.clipboard.writeText(`${nameVal["category"]}_${nameVal["name"]}_${nameVal["size"]}_${nameVal["roughness"]}`);
+}
 
 //   documentation: https://github.com/image-js/image-js#examples-in-the-browser
 
